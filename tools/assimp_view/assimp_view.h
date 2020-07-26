c6c5cab38b3f37b857f43f8810ea21dd1645024b
Andrew Johnson <ajohnson@draster.com>
2020-07-25 19:40:39 -0700

    Make FPS camera movement speed 10x slower.

--- tools/assimp_view/assimp_view.h
+++ tools/assimp_view/assimp_view.h
@@ -76,7 +76,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <time.h>
 
 // default movement speed
-#define MOVE_SPEED 0.003f
+#define MOVE_SPEED 0.3f
 
 #include "AssetHelper.h"
 #include "Background.h"
